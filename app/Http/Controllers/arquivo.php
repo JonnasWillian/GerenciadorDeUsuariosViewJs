6e02d49a461fe25a1c8d0f89c7cf3d1ed43a1a20
JonnasWillian <jonnasnogueira2@gmail.com>
2025-05-06 22:35:57 -0300

    estrutura de anexo

---
+++ app/Http/Controllers/arquivo.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\arquivo AS ArquivoModel;
+
+class arquivo extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $arquivos = ArquivoModel::where('user_id', $request->user_id)->get();
+        
+        return response()->json($arquivos);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'arquivo' => 'required|file',
+        ]);
+
+        $caminho = $request->file('arquivo')->store('arquivos', 'public');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
